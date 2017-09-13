68a64f039f2d1be08c3b8e3fc6dd65e382bbfe59
Abel Simon <abel.simon1@marist.edu>
2017-09-13 17:28:39 -0400

    List of comments describing what the program seeks to achieve and how it will achieve it, line by line

---
+++ project1.py
@@ -0,0 +1,15 @@
+# Set player location to 1
+# Set player score to 0
+# Use input function to show a game start message that introduces the main storyline and prompts the player to press enter to continue
+# Set player location to 2
+# Add 5 to player score and display it
+# Display a message describing their new location and prompt them to press enter again to continue
+# Set player location to 3
+# Add 5 to player score and display it
+# Display a message describing their new location and prompt them to press enter again to continue
+# Set player location to 4
+# Add 5 to player score
+# Display a message describing their new location and prompt them to press enter again to complete the game
+# Show the player their score.
+
+
