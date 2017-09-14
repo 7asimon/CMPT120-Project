cbe697e9001b9cad09273c6091b7ba58433dd9bf
Abel Simon <abel.simon1@marist.edu>
2017-09-14 15:10:43 -0400

    Completely reworked code: 1. All locations are now defined as variables at the very start of the code and are later called in order. 2. Current score is now displayed to the player every time they reach a new stage. 3. Instead of the score being manually set to be 5 points higher (5, 10, 15, 20) like it was before, 5 is simply added to the score variable every time the player progresses. 4. Line breaks added in key points to improve readability.

--- project1.py
+++ project1.py
@@ -1,36 +1,55 @@
-# Set player location to 1
-# Set player score to 0
-# Use input function to show a game start message that introduces the main storyline and prompts the player to press enter to continue
-# Set player location to 2
-# Add 5 to player score and display it
-# Display a message describing their new location and prompt them to press enter again to continue
-# Set player location to 3
-# Add 5 to player score and display it
-# Display a message describing their new location and prompt them to press enter again to continue
-# Set player location to 4
-# Add 5 to player score
-# Display a message describing their new location and prompt them to press enter again to complete the game
-# Show the player their score.
-
-
-location = 0
 score = 0
-print("GREYWALKER")
-input("You are a sleepwalker and an insomniac who is attempting to master lucid dreaming. You are currently stuck in a dream; aware that you are in a dream state but unable to control your actions. Press Enter to continue...")
-location = 1
-score = 5
-input("In your dream, you are walking forward in a meadow littered with wilted, white dasies. Smoke fills the air and the sky is tinted an eerily familiar shade of red. You try to sieze control of your body and set your own course, but your efforts prove futile. Your body continues to run forward with purpose despite there being nothing in sight other than the dasies in this seemingly endless field. Press ENTER to continue...")
-location = 2
-score = 10
-input("After what felt like hours of non-stop running, you come across a strange figure eating alone at a small table. Upon venturing closer, you realize that the person sitting there is a mirror image of yourself. You are terrified by this and wish to run away, but your body goes and sits down across from him against your will. Press ENTER to continue.")
-location = 3
-score = 15
-input("Suddenly, the entire meadow begins to fold in on itself as the color changes to a bleak grey. You watch as the color also drains from yourself and your surroundings until everything in your dream is black and white. \" You're certainly wondering where you are, aren't you? What's the matter, does it feel too real to be another random dream?\" the mirror image angrily asks of you. He lifts his fork from the plate and forcefully stabs it into the table. The letters he begins carving are rough and jagged, but when he finishes you can clearly make out the words \" D E A T H  R O W \". Press ENTER to continue.")
-location = 4
-score = 20
-input("Upon reading the words, you violently jolted up from your bed. You had experienced strange dreams before, but the meadow felt far too familiar for it to be pure coincidence. You knew you had to lucid dream and re-enter that strange world so that you could figure out what happened and the reason for the carving on the table. Press ENTER to complete the game.")
-print("To Be Continued In Future Game Versions...")
-print("Final Score:", score)
+gamestart = ("You are a sleepwalker and an insomniac who is attempting to master lucid dreaming. You are currently stuck in a dream;"
+" aware that you are in a dream state but unable to control your actions. You begin with a score of 0 and you will gain 5 points for every stage you progress through.")
+meadow = ("In your dream, you are walking forward in a meadow littered with wilted, white dasies. Smoke fills the air and the sky is tinted an eerily familiar shade of red."
+" You try to sieze control of your body and set your own course, but your efforts prove futile."
+" Your body continues to run forward with purpose despite there being nothing in sight other than the dasies in this seemingly endless field.")
+table = ("After what felt like hours of non-stop running, you come across a strange figure eating alone at a small table."
+" Upon venturing closer, you realize that the person sitting there is a mirror image of yourself."
+" You are terrified by this and wish to run away, but your body goes and sits down across from him against your will.")
+greyroom = ("Suddenly, the entire meadow begins to fold in on itself as the color changes to a bleak grey."
+" You watch as the color also drains from yourself and your surroundings until everything in your dream is black and white."
+" \" You're certainly wondering where you are, aren't you?\" the mirror image asks of you, barely able to control his laughter."
+" He lifts his fork from the plate and forcefully stabs it into the table."
+" The letters he begins carving are rough and jagged, but when he finishes you can clearly make out the words \" D E A T H  R O W \".")
+dreamend = ("Upon reading the words, you violently jolted up from your bed. You had experienced strange dreams before, but the meadow felt far too familiar for it to be pure coincidence."
+" You knew you had to lucid dream and re-enter that strange world so that you could figure out what happened and the reason for the carving on the table."
+" To Be Continued In Future Game Versions...")
 
+location = gamestart
+print("GREYWALKER")
+print('')
+print(location)
+print('')
+input("Press ENTER to continue...")
+print('')
+score = score + 5
+location = meadow
+print(location)
+print('')
+print("Your score is ", score)
+input("Press ENTER to continue...")
+print('')
+score = score + 5
+location = table
+print(location)
+print('')
+print("Your score is ", score)
+input("Press ENTER to continue...")
+print('')
+score = score + 5
+location = greyroom
+print(location)
+print('')
+print("Your score is ", score)
+input("Press ENTER to continue...")
+print('')
+score = score + 5
+location = dreamend
+print(location)
+print('')
+print("Your final score is ", score)
+input("Press ENTER to to complete the game")
+print('')
 
 print("Copyright: Abel Simon, abel.simon1@marist.edu")
