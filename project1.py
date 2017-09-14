b9ebb998125ca483b028943f72de3367b4027188
Abel Simon <abel.simon1@marist.edu>
2017-09-13 20:24:10 -0400

    All stages complete

--- project1.py
+++ project1.py
@@ -15,7 +15,7 @@
 
 location = 0
 score = 0
-input("You are an insomniac who is attempting to master lucid dreaming. You are currently stuck in a dream; aware that you are in a dream state but unable to control your actions. Press Enter to continue...")
+input("You are a sleepwalker and an insomniac who is attempting to master lucid dreaming. You are currently stuck in a dream; aware that you are in a dream state but unable to control your actions. Press Enter to continue...")
 location = 1
 score = 5
 input("In your dream, you are walking forward in a meadow littered with wilted, white dasies. Smoke fills the air and the sky is tinted an eerily familiar shade of red. You try to sieze control of your body and set your own course, but your efforts prove futile. Your body continues to run forward with purpose despite there being nothing in sight other than the dasies in this seemingly endless field. Press ENTER to continue...")
@@ -24,4 +24,8 @@ score = 10
 input("After what felt like hours of non-stop running, you come across a strange figure eating alone at a small table. Upon venturing closer, you realize that the person sitting there is a mirror image of yourself. You are terrified by this and wish to run away, but your body goes and sits down across from him against your will. Press ENTER to continue.")
 location = 3
 score = 15
-input("Suddenly, the entire meadow begins to fold in on itself as the color changes to a bleak grey. You watch as the color also drains from yourself and your surroundings until everything in your dream is black and white. \" You're certainly wondering what I'm - or rather - what YOU'RE doing here, aren't you?\" the mirror image says to you. 
+input("Suddenly, the entire meadow begins to fold in on itself as the color changes to a bleak grey. You watch as the color also drains from yourself and your surroundings until everything in your dream is black and white. \" You're certainly wondering where you are, aren't you? What's the matter, does it feel too real to be another random dream?\" the mirror image angrily asks of you. He lifts his fork from the plate and forcefully stabs it into the table. The letters he begins carving are rough and jagged, but when he finishes you can clearly make out the words \" D E A T H  R O W \". Press ENTER to continue.")
+location = 4
+score = 20
+input("Upon reading the words, you violently jolted up from your bed. You had experienced strange dreams before, but the meadow felt far too familiar for it to be pure coincidence. You knew you had to lucid dream and re-enter that strange world so that you could figure out what happened and the reason for the carving on the table. Press ENTER to complete the game.")
+
