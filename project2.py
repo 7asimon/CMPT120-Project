f86066695863a4c63b9fc1b5058c4d2f5e59dd8b
Abel Simon <abel.simon1@marist.edu>
2017-09-29 14:46:24 -0400

    Work being done on gameLoop

--- project2.py
+++ project2.py
@@ -1,17 +1,9 @@
-meadow = ("In your dream, you are walking forward in a meadow littered with wilted, white dasies. Smoke fills the air and the sky is tinted an eerily familiar shade of red."
-" You try to sieze control of your body and set your own course, but your efforts prove futile."
-" Your body continues to run forward with purpose despite there being nothing in sight other than the dasies in this seemingly endless field.")
+meadow = ("this shit flowery")
 burningVillage = ("this shit burned")
 greyRoom = ("this shit grey")
 dinnerTable = ("eat shit here")
 cliff = ("dont jump off this shit")
 houseInterior = ("strange shit going on in here")
-
-stage1 = False
-stage2 = False
-stage3 = False
-stage4 = False
-stage6 = False
 
 score = 0
 
@@ -24,6 +16,10 @@ def gameIntro():
     print('')
     input(" You begin with a score of 0 and you will gain 5 points for every stage you progress through."
           " Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again. For now, press ENTER to continue.")
+    stage1 = False
+    if stage1 == False:
+        scoreLocation()
+        stage1 == True
 
 def gameEnd():
     print("You knew you had no choice but to lucid dream and re-enter that strange world so that you could figure out what happened and the reason for the carving on the table."
@@ -32,45 +28,76 @@ def gameEnd():
     
 
 def gameLoop():
-    if stage1 == False:
+    stage1 = False
+    stage2 = False
+    stage3 = False
+    stage4 = False
+    stage6 = False
+    loop = 1
+    while True:
+        while loop == 1:
+            location = meadow
+            print("MEADOW")
+            print("Score:",score)
+            print(location)
+            userChoice = input()
+            if userChoice == ("North"):
+                loop = 2
+                break
+            if userChoice == ("South"):
+                loop = 3
+                break
+            if userChoice == ("East"):
+                loop = 4
+                break
+            if userChoice == ("West"):
+                loop = 5
+                break
+            if userChoice == ("Help"):
+                print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
+                print('')
+                loop = 1
+                break
+            if userChoice == ("Quit"):
+                quit(1)
+            else:
+                print("Please enter a valid command")
+                print('')
+                loop = 1
+                break
+                
+        while loop == 2:
+            location = burningVillage
+            if stage2 == False:
+                scoreLocation()
+            stage2 == True
+            print("BURNING VILLAGE")
+            print("Score:",score)
+            print(location)
+            userChoice = input()
+            if userChoice == ("South"):
+                loop = 1
+                break
+            if userChoice == ("North"):
+                loop = 4
+
+        while loop == 3:
+            location = cliff
+            if stage3 == False:
+                storeLocation()
+            stage3 == True
+            print("CLIFF"
+            print("Score:",score)
+            print(location)
+            userChoice = input()
+            if userChoice = 
+            
+
+def scoreLocation():
         global score
         score = score + 5
-        stage1 == True
-    loop = 1
-    while loop == 1:
-        location = meadow
-        print("MEADOW")
-        print("Score:",score)
-        print(location)
-        userChoice = input()
-        if userChoice == ("North"):
-            loop = 2
-            break
-        if userChoice == ("South"):
-            print("There is no desirable path to the south.")
-            loop = 1
-        if userChoice == ("Help"):
-            print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
-            loop = 1
-        if userChoice == ("Quit"):
-            quit(1)
-        else:
-            print("Please enter a valid command")
-            loop = 1
-            
-    while loop == 2:
-        location = burningVillage
-        if stage2 == False:
-            score = score + 5
-            stage2 == True
-        print("BURNING VILLAGE")
-        print("Score:",score)
-        print(location)
-        userChoice = input()
-        if userChoice == ("South"):
-            loop = 1
-            gameLoop()
-
+        
+    
             
 gameIntro() 
 gameLoop()
