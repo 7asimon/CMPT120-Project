5c2ee06ca8293c0647598b4068659d8599439674
Abel Simon <abel.simon1@marist.edu>
2017-10-26 19:11:48 -0400

    messing around with game loop and goTo function to try and find a working solution

--- project3.py
+++ project3.py
@@ -4,12 +4,23 @@
 
 score = 0
 
-def gameIntro():
+def showIntro():
+    print("GREYWALKER")
+    print('')
+    playerName = input("Your name is...you cannot seem to recall it. Try to remember your name: ")
+    print("Ah, yes, " + playerName + ", that was it. You are a sleepwalker"
+          "and an insomniac who is attempting to master lucid dreaming."
+    "You have successfully assumed control of yourself within your dream and intend to explore the strange realm you have dreamed of.")
+    print('')
+    input(" You begin with a score of 0 and you will gain 5 points for every stage you progress through."
+          "You will win the game when all locations have been visited."
+          " Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again."
+          " For now, press ENTER to continue. ")
     
 
-location = ["As the dream begins, you find yourself in a vaguely familiar meadow filled with wilted daisies."
-          "Looking forward, you can see a village engulfed in flames."
-          "There appears to be areas of interest to the east, west, and south as well.", "The village is completely vacant as the buildings crumble and burn around you."
+location = ["You find yourself in a vaguely familiar meadow filled with wilted daisies."
+          " Looking forward, you can see a village engulfed in flames."
+          " There appears to be areas of interest to the east, west, and south as well.", "The village is completely vacant as the buildings crumble and burn around you."
                   "However, in front of you lies one house that is completely unaffected by the fire,"
                   "and a sign in front of it that reads: WHY HAVE YOU CAUSED US SUCH AGONY?", "You arrive in a grey room with random burned objects scattered about."
             "As you walk into the room, the color fades from your skin and you notice everything you see is in black and white." ,
@@ -23,14 +34,8 @@ location = ["As the dream begins, you find yourself in a vaguely familiar meadow
 
 stage = [False, False, False, False, False, False, False, False]
 
-meadow = ()
-burningVillage = ()
-greyRoom = ()
-dinnerTable = ()
-cliff = ()
-houseInterior = ()
-strangeWall = ()
-villageSide = () 
+curLocation = location[0]
+ 
 def gameEnd():
     print("Suddenly, before you could discover all you needed to, you awaken from the dream."
           "You knew you had no choice but to lucid dream and re-enter that strange world so that you could figure out what happened and the reason for the cryptic message on the sign"
@@ -38,258 +43,48 @@ def gameEnd():
     input("Copyright: Abel Simon, abel.simon1@marist.edu")
     quit(1)
 
-def placeHolder():
-    stage[0] = True location[0]
+
 
 def gameLoop():
-    loop = 1
-    while True:
-        while loop == 1:
-            location = meadow
-            if stage1 == False:
-                scoreLocation()
-            stage1 = True
-            print("MEADOW")
-            print("Score:",score)
-            print(location)
-            if score == 30:
-                gameEnd()
-            userChoice = input()
-            if userChoice == ("North"):
-                loop = 2
-                break
-            if userChoice == ("South"):
-                loop = 3
-                break
-            if userChoice == ("East"):
-                loop = 4
-                break
-            if userChoice == ("West"):
-                loop = 5
-                break
-            if userChoice == ("Help"):
-                print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
-                print('')
-                break
-            if userChoice == ("Quit"):
-                quit(1)
-            else:
-                print("Please enter a valid command")
-                print('')
-                loop = 1
-                break
-                
-        while loop == 2:
-            location = burningVillage
-            if stage2 == False:
-                scoreLocation()
-            stage2 = True
-            print("BURNING VILLAGE")
-            print("Score:",score)
-            print(location)
-            if score == 30:
-                gameEnd()
-            userChoice = input()
-            if userChoice == ("South"):
-                loop = 1
-                break
-            if userChoice == ("North"):
-                loop = 6
-                break
-            if userChoice == ("East"):
-                print("There is no desirable path to the east.")
-                print('')
-                break
-            if userChoice == ("West"):
-                print("There is no desirable path to the west.")
-                print('')
-                break
-            if userChoice == ("Help"):
-                print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
-                print('')
-                break
-            if userChoice == ("Quit"):
-                quit(1)
-            else:
-                print("Please enter a valid command")
-                print('')
-                break
-
-        while loop == 3:
-            location = cliff
-            if stage3 == False:
-                scoreLocation()
-            stage3 = True
-            print("CLIFF")
-            print("Score:",score)
-            print(location)
-            if score == 30:
-                gameEnd()
-            userChoice = input()
-            if userChoice == ("North"):
-                loop = 1
-                break
-            if userChoice == ("South"):
-                  suicide = input("Continuing to go South will send you off the cliff and kill you. Are you sure you wish to die? (Y/N)")
-                  if suicide == ("Y") or suicide == ("y"):
-                      print("You have died within the dream and it has ended.")
-                      quit(1)
-                  else:
-                      loop = 3
-                      break
-            if userChoice == ("East"):
-                print("There is no desirable path to the east.")
-                print('')
-                break
-            if userChoice == ("West"):
-                print("There is no desirable path to the west.")
-                print('')
-                break
-            if userChoice == ("Help"):
-                print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
-                print('')
-                break
-            if userChoice == ("Quit"):
-                quit(1)
-            else:
-                print("Please enter a valid command")
-                print('')
-                break
-        while loop == 4:
-            location = greyRoom
-            if stage4 == False:
-                scoreLocation()
-            stage4 = True
-            print("GREY ROOM")
-            print("Score:",score)
-            print(location)
-            if score == 30:
-                gameEnd()
-            userChoice = input()
-            if userChoice == ("North"):
-                print("There is no desirable path to the north.")
-                print('')
-                break
-            if userChoice == ("South"):
-                print("There is no desirable path to the south.")
-                print('')
-                break
-            if userChoice == ("East"):
-                print("There is no desirable path to the east.")
-                print('')
-                break
-            if userChoice == ("West"):
-                loop = 1
-                break
-            if userChoice == ("Help"):
-                print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
-                print('')
-                break
-            if userChoice == ("Quit"):
-                quit(1)
-            else:
-                print("Please enter a valid command")
-                print('')
-                break
-        while loop == 5:
-            location = dinnerTable
-            if stage5 == False:
-                scoreLocation()
-            stage5 = True
-            print("DINNER TABLE")
-            print("Score:",score)
-            print(location)
-            if score == 30:
-                gameEnd()
-            userChoice = input()
-            if userChoice == ("North"):
-                print("There is no desirable path to the north.")
-                print('')
-                break
-            if userChoice == ("South"):
-                print("There is no desirable path to the south.")
-                print('')
-                break
-            if userChoice == ("East"):
-                loop = 1
-                break
-            if userChoice == ("West"):
-                print("There is no desirable path to the west.")
-                print('')
-                break
-            if userChoice == ("Help"):
-                print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
-                print('')
-                break
-            if userChoice == ("Quit"):
-                quit(1)
-            else:
-                print("Please enter a valid command")
-                print('')
-                break
-        while loop == 6:
-            location = houseInterior
-            if stage6 == False:
-                scoreLocation()
-            stage6 = True
-            print("HOUSE INTERIOR")
-            print("Score:",score)
-            print(location)
-            if score == 30:
-                gameEnd()
-            userChoice = input()
-            if userChoice == ("North"):
-                print("There is no desirable path to the north.")
-                print('')
-                break
-            if userChoice == ("South"):
-                loop = 2
-                break
-            if userChoice == ("East"):
-                print("There is no desirable path to the east.")
-                print('')
-                break
-            if userChoice == ("West"):
-                print("There is no desirable path to the west.")
-                print('')
-                break
-            if userChoice == ("Help"):
-                print("Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again.")
-                print('')
-                break
-            if userChoice == ("Quit"):
-                quit(1)
-            else:
-                print("Please enter a valid command")
-                print('')
-                break
+   global curLocation
+   global location
+   global score
+   global stage
+   while True:
+       print(curLocation)
+       userChoice = input()
+       if userChoice == "north":
+           if curLocation == location[0]:
+               goTo(1)
+           if curLocation == location[1]:
+               goTo(2)
+           if curLocation == location[2]:
+               goTo(3)
+           if curLocation == location[3]:
+               goTo(4)
+           if curLocation == location[4]:
+               goTo(5)
+           if curLocation == location[5]:
+               goTo(6)
+           if curLocation == location[6]:
+               goTo(7)
+           if curLocation == location[7]:
+               goTo(8)
+    
             
 
 def goTo(x):
+    global curLocation
+    global stage
+    global score
+    curLocation = location[x]
+    if stage is False:
+        score = score + 5
+        print(score)
        
 
 def main():
-    print("GREYWALKER")
-    print('')
-    playerName = input("Your name is...you cannot seem to recall it. Try to remember your name: ")
-    print("Ah, yes, " + playerName + ", that was it. You are a sleepwalker"
-          "and an insomniac who is attempting to master lucid dreaming."
-    "You have successfully assumed control of yourself within your dream and intend to explore the strange realm you have dreamed of.")
-    print('')
-    input(" You begin with a score of 0 and you will gain 5 points for every stage you progress through."
-          "You will win the game when all locations have been visited."
-          " Type North, South, East, or West to navigate, type Quit to end the game, or type Help to view this message again."
-          " For now, press ENTER to continue. ")
+    showIntro()
     gameLoop()
-    
+
 main()
-
-goTo(i)
- global
- location[i]
-
-if not hasBeenThere[i]
- # add to score
-
-hasBeenThere[i] = True
-
