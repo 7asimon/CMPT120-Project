167513af480a13e555f0ea935a304f790474931f
Abel Simon <abel.simon1@marist.edu>
2017-10-26 20:55:51 -0400

    required loops completed and functional

--- project3.py
+++ project3.py
@@ -20,17 +20,20 @@ def showIntro():
 
 location = ["You find yourself in a vaguely familiar meadow filled with wilted daisies."
           " Looking forward, you can see a village engulfed in flames."
-          " There appears to be areas of interest to the east, west, and south as well.", "The village is completely vacant as the buildings crumble and burn around you."
-                  "However, in front of you lies one house that is completely unaffected by the fire,"
-                  "and a sign in front of it that reads: WHY HAVE YOU CAUSED US SUCH AGONY?", "You arrive in a grey room with random burned objects scattered about."
+          " There appears to be areas of interest to the east, west, and south as well.",
+            "The village is completely vacant as the buildings crumble and burn around you."
+            "However, in front of you lies one house that is completely unaffected by the fire,"
+            "and a sign in front of it that reads: WHY HAVE YOU CAUSED US SUCH AGONY?",
+            "You arrive in a grey room with random burned objects scattered about."
             "As you walk into the room, the color fades from your skin and you notice everything you see is in black and white." ,
              "You arrive at a dinner table with a mirror image of yourself. No matter what you do, he does not speak to you.",
              "You arrive at the edge of a cliff with a dark abyss below it. There is nothing of interest here.",
              "Inside the house, you find that the walls are covered with black and white pictures of your family"
-                 "that were taken before they died mysteriously years ago."
-                 "Eerily enough, you are missing from all the pictures as if you were cropped out of them.",
+                "that were taken before they died mysteriously years ago."
+                "Eerily enough, you are missing from all the pictures as if you were cropped out of them.",
              "You come across a wall with seemingly meaningless inscribings on them. You cannot make out what the strange drawings say.",
-             "You enter another part of the village that is blocked off from the other side. There appears to be nothing of interest here."]
+             "Walking down the path, you witness many villagers screaming in agony and running down the street."
+             "There appears to be an endless number of them"]
 
 hasBeenThere = [False, False, False, False, False, False, False, False]
 
@@ -46,69 +49,108 @@ def gameEnd():
 
 
 def gameLoop():
-   global curLocation
-   global location
-   global score
-   global stage
-   while True:
-       print(curLocation)
-       userChoice = input()
-       userChoice = userChoice.lower()
-       if userChoice == ("north"):
-           if curLocation == location[0]:
-               goTo(2)
-           elif curLocation == location[1]:
-               goTo(3)
-           elif curLocation == location[2]:
-               goTo(5)
-           elif curLocation == location[3]:
-               goTo(6)
-           elif curLocation == location[4]:
-               goTo(7)
-           elif curLocation == location[5]:
-               print("That is not a valid direction from your currrent location")
-           elif curLocation == location[6]:
-               print("That is not a valid direction from your currrent location")
-           else:
-               print("That is not a valid direction from your currrent location")
-        if userChoice == ("south")
-            if curLocation == location[0]:
-                print("That is not a valid direction from your currrent location")
-            elif curLocation == location[1]:
-                print("That is not a valid direction from your currrent location")
-            elif curLocation == location[2]:
-                goTo(0)
-            elif curLocation == location[3]:
-                goTo(1)
-            elif curLocation == location[4]:
-                print("That is not a valid direction from your currrent location")
-            elif curLocation == location[5]:
-                goTo(2)
-            elif curLocation == location[6]:
-                goTo(3)
-            elif curLocation == location[7]:
-                goTo(4)
-            else:
-                print("That is not a valid direction from your currrent location")
-        if userChoice == ("west")
-            if curLocation == location[0]:
-                print("That is not a valid direction from your currrent location")
-            elif curLocation == location[1]:
-                goTo(0)
-            elif curLocation == location[2]:
-                print("That is not a valid direction from your currrent location")
-            elif curLocation == location[3]:
-                goTo(2)
-            elif curLocation == location[4]:
-                goTo(3)
-            elif curLocation == location[5]:
-                print("That is not a valid direction from your currrent location")
-            elif curLocation == location[6]:
-                goTo(5)
-            elif curLocation == location[7]:
-                goTo(6)
-    
-            
+        global curLocation
+        global location
+        global score
+        global stage
+        while True:
+           print(curLocation)
+           userChoice = input()
+           userChoice = userChoice.lower()
+           if userChoice == ("north"):
+               if curLocation == location[0]:
+                   goTo(2)
+               elif curLocation == location[1]:
+                   goTo(3)
+               elif curLocation == location[2]:
+                   goTo(5)
+               elif curLocation == location[3]:
+                   goTo(6)
+               elif curLocation == location[4]:
+                   goTo(7)
+               elif curLocation == location[5]:
+                   print("That is not a valid direction from your currrent location")
+               elif curLocation == location[6]:
+                   print("That is not a valid direction from your currrent location")
+               else:
+                   print("That is not a valid direction from your currrent location")
+           if userChoice == ("south"):
+                if curLocation == location[0]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[1]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[2]:
+                    goTo(0)
+                elif curLocation == location[3]:
+                    goTo(1)
+                elif curLocation == location[4]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[5]:
+                    goTo(2)
+                elif curLocation == location[6]:
+                    goTo(3)
+                elif curLocation == location[7]:
+                    goTo(4)
+                else:
+                    print("That is not a valid direction from your currrent location")
+           if userChoice == ("west"):
+                if curLocation == location[0]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[1]:
+                    goTo(0)
+                elif curLocation == location[2]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[3]:
+                    goTo(2)
+                elif curLocation == location[4]:
+                    goTo(3)
+                elif curLocation == location[5]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[6]:
+                    goTo(5)
+                elif curLocation == location[7]:
+                    goTo(6)
+                else:
+                    print("That is not a valid direction from your currrent location")
+           if userChoice == ("east"):
+                if curLocation == location[0]:
+                    goTo(1)
+                elif curLocation == location[1]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[2]:
+                    goTo(3)
+                elif curLocation == location[3]:
+                    goTo(4)
+                elif curLocation == location[4]:
+                    print("That is not a valid direction from your currrent location")
+                elif curLocation == location[5]:
+                    goTo(6)
+                elif curLocation == location[6]:
+                    goTo(7)
+                elif curLocation == location[7]:
+                    print("That is not a valid direction from your currrent location")
+           if userChoice == ("score"):
+                print(score)
+           if userChoice == ("help"):
+                print("Type North, South, East, or West to navigate, type Map to view the map, type Points to view your score,"
+                      " or type Quit to end the game."
+                      " You may also type Help at any point to view this message again.")
+           if userChoice == ("quit"):
+               quit(1)
+           if userChoice == ("map"):
+                print(
+                       "House Interior ============= Strange Wall ============== Path of Agony \n"
+                       "     ||                           ||                          ||       \n"
+                       "     ||                           ||                          ||       \n"
+                       "     ||                           ||                          ||       \n"
+                       "     ||                           ||                          ||       \n"
+                       " Grey Room ================ Dinner Table =================== Cliff     \n"
+                       "     ||                           ||                                   \n"                  
+                       "     ||                           ||                                   \n"
+                       "     ||                           ||                                   \n"
+                       "     ||                           ||                                   \n"
+                       "   Meadow ==================== Village                                 \n"
+                      )
 
 def goTo(x):
     global curLocation
@@ -118,7 +160,9 @@ def goTo(x):
     if hasBeenThere[x] is False:
         score = score + 5
         print(score)
-       
+        hasBeenThere[x] = True
+    if score == 40:
+        gameEnd()
 
 def main():
     showIntro()
